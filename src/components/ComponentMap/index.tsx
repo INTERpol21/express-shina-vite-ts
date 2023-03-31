e86dda25fc27da45cffbae6b0759e521d8b275c8
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2023-03-31 14:49:59 +0600

    v2

--- src/components/ComponentMap/index.tsx
+++ src/components/ComponentMap/index.tsx
@@ -1,4 +1,3 @@
-
 import styles from './index.module.scss'
 import { Map, Placemark, YMaps, FullscreenControl, ZoomControl } from '@pbe/react-yandex-maps'
 import { MapProps } from '../../types'
@@ -7,7 +6,7 @@ const ComponentMap = ({ coordinate, zoom }: MapProps) => {
   return (
     <YMaps>
       <Map
-        className={styles.map}
+        className={styles.mapYandex}
         state={{
           center: coordinate || [54.708415, 55.975993],
           zoom: zoom,
--- src/components/ComponentSidebar/index.tsx
+++ src/components/ComponentSidebar/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styles from './index.module.scss'
 import { Scrollbars } from 'react-custom-scrollbars-2'
 import { Navigate, SidebarProps } from '../../types'
@@ -6,35 +5,33 @@ import { Navigate, SidebarProps } from '../../types'
 const ComponentSidebar = ({ setCoordinate, setZoom, places }: SidebarProps) => {
   const handleClick = (place: Navigate) => {
     setCoordinate([place.latitude, place.longitude])
-    setZoom(15)
+    setZoom(16)
   }
 
   return (
     <div className={styles.sidebar}>
-      <div className={styles.sidebar__container}>
-        <Scrollbars style={{ height: '95vh' }}>
-          {places.map((place) => {
-            return (
-              <div
-                className={styles.sidebar__items}
-                key={place.latitude}
-                tabIndex={0}
-                onClick={() => handleClick(place)}>
-                <div className={styles.sidebar__content_address}>{place.address}</div>
-                <div className={styles.sidebar__content_buttons}>
-                  {place.budgets.map((budget, index) => {
-                    return (
-                      <div className={styles.sidebar__content_button} key={budget + index}>
-                        {budget}
-                      </div>
-                    )
-                  })}
-                </div>
+      <Scrollbars style={{ height: '95vh' }}>
+        {places.map((place) => {
+          return (
+            <div
+              className={styles.sidebar__items}
+              key={place.latitude}
+              tabIndex={0}
+              onClick={() => handleClick(place)}>
+              <div className={styles.sidebar__content_address}>{place.address}</div>
+              <div className={styles.sidebar__content_buttons}>
+                {place.budgets.map((budget) => {
+                  return (
+                    <div className={styles.sidebar__content_button} key={budget}>
+                      {budget}
+                    </div>
+                  )
+                })}
               </div>
-            )
-          })}
-        </Scrollbars>
-      </div>
+            </div>
+          )
+        })}
+      </Scrollbars>
     </div>
   )
 }
